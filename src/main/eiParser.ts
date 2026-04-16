c4be437842bc8bebc0f486eb186cbf04f30ba0ff
darkharasho <144265798+darkharasho@users.noreply.github.com>
2026-04-15 22:56:40 -0700

    feat(ei): flip parseCombatReplay default to true

--- src/main/eiParser.ts
+++ src/main/eiParser.ts
@@ -30,7 +30,7 @@ export const DEFAULT_EI_SETTINGS: EiParserSettings = {
     anonymous: false,
     customTooShort: 2200,
     saveOutHTML: false,
-    parseCombatReplay: false,
+    parseCombatReplay: true,
     lightTheme: false,
     rawTimelineArrays: true,
     singleThreaded: false,
